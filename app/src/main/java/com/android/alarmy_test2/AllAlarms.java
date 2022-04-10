b0d478bdbc2ac2cf739e9ad9a3994a3f3c23c36c
Nguyen <dipien2408@gmail.com>
2022-04-10 13:54:15 +0700

    Home alarm commit

---
+++ app/src/main/java/com/android/alarmy_test2/AllAlarms.java
@@ -0,0 +1,14 @@
+package com.android.alarmy_test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllAlarms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_alarms);
+    }
+}
