9fd1faa90b1fc43cd45734234787ef59a9569830
Nguyen <dipien2408@gmail.com>
2022-04-10 09:23:04 +0700

    Home alarm commit

--- app/src/main/java/com/android/alarmy_test2/HomeFragment.java
+++ app/src/main/java/com/android/alarmy_test2/HomeFragment.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        FloatingActionButton fab1 = (FloatingActionButton) v.findViewById(R.id.fab_1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeFragment.this.getActivity(), "fab1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return v;
     }
+
 }
