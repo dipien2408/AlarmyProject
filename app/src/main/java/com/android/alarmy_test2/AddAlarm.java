cc3bd77ec92d7d2bfa09ac906d83ccc63ef53a63
Giang <giangnguyen010801@gmail.com>
2022-04-27 09:45:13 +0700

    Update alarm Event

--- app/src/main/java/com/android/alarmy_test2/AddAlarm.java
+++ app/src/main/java/com/android/alarmy_test2/AddAlarm.java
@@ -9,6 +9,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -37,7 +38,8 @@ public class AddAlarm extends AppCompatActivity {
     Button btnSave;
     AlarmManager alarmManager;
     PendingIntent pendingIntent;
-    List<Button> buttons = new ArrayList<Button>();
+    ArrayList<Button> buttons = new ArrayList<Button>();
+    ArrayList<Boolean> checkBtn = new ArrayList<Boolean>();
     Boolean True = true;
 
     @Override
@@ -49,15 +51,18 @@ public class AddAlarm extends AppCompatActivity {
         timeEstimate = findViewById(R.id.timeEstimate);
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         if (buttons.size() == 0) {
-            buttons.add((Button) findViewById(R.id.sundayBtn));
             buttons.add((Button) findViewById(R.id.mondayBtn));
             buttons.add((Button) findViewById(R.id.tuesdayBtn));
             buttons.add((Button) findViewById(R.id.wednesdayBtn));
             buttons.add((Button) findViewById(R.id.thursdayBtn));
             buttons.add((Button) findViewById(R.id.fridayBtn));
             buttons.add((Button) findViewById(R.id.saturdayBtn));
+            buttons.add((Button) findViewById(R.id.sundayBtn));
         }
         Intent intent = new Intent(AddAlarm.this, AlarmReceiver.class);
+        Intent intentBtn = new Intent(AddAlarm.this, MainActivity.class);
+        Bundle bundle = new Bundle();
+
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,36 +97,41 @@ public class AddAlarm extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
 
             @Override
+            // Kiem tra lich bao thuc
             public void onClick(View view) {
-                calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
-                calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
-                Calendar curCalendar = Calendar.getInstance();
-                int currentHour = curCalendar.get(Calendar.HOUR_OF_DAY);
-                int currentMinute = curCalendar.get(Calendar.MINUTE);
-                int hour = timePicker.getCurrentHour();
-                int minute = timePicker.getCurrentMinute();
-                Log.d("Current", String.valueOf(currentHour) + " giờ " + String.valueOf(currentMinute));
-                Log.d("Set time", String.valueOf(hour) + " giờ " + String.valueOf(minute));
-                pendingIntent = PendingIntent.getBroadcast(AddAlarm.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+//                for (int i = 0; i < buttons.size(); i++) {
+//                    int temp=i+1;
+//                    if(buttons.get(i).getTag()==True ){
+//                        calendar.set(Calendar.DAY_OF_WEEK, temp);
+//                        Log.d("Kiem tra lich bao thuc", String.valueOf(temp));
+//                        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
+//                        calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+//                        pendingIntent = PendingIntent.getBroadcast(AddAlarm.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+//                    }
+//                }
 //                int hourEstimate = hour-currentHour;
 //                int minuteEstimate = minute-currentMinute;
 //                if (hourEstimate < 0 ){
 //                    hourEstimate = 24 + hourEstimate;
 //                }
-//
 //                String HourEstimate = String.valueOf(hourEstimate);
 //                String MinuteEstimate = String.valueOf(minute-currentMinute);
 //                timeEstimate.setText("Báo thức được đặt trong " +HourEstimate+" giờ "+MinuteEstimate+" phút tính từ bây giờ");
-
-
+                for (int i = 0; i < buttons.size(); i++) {
+                    checkBtn.add((Boolean) buttons.get(i).getTag());
+                }
+                intentBtn.setAction(Intent.ACTION_SEND);
+                intentBtn.putExtra("list",checkBtn);
+                intentBtn.putExtra("hour",timePicker.getCurrentHour());
+                intentBtn.putExtra("minute",timePicker.getCurrentMinute());
+                startActivity(intentBtn);
             }
         });
     }
 
 
     public void takeBtn(View view) {
-        Log.d("Take Button", this.toString());
         int index = 0;
         for (int i = 0; i < buttons.size(); i++) {
             if (buttons.get(i).getId() == view.getId()) {
@@ -133,13 +143,8 @@ public class AddAlarm extends AppCompatActivity {
                     buttons.get(i).setTag(false);
                     buttons.get(i).setBackgroundColor(ContextCompat.getColor(this, R.color.backgroundbtn_alarm));
                 }
-                Log.d("Index", String.valueOf(index) + buttons.get(i).getTag());
-
                 break;
             }
-        }
-        for (int i = 0; i < buttons.size(); i++) {
-            Log.d("Checked ", String.valueOf(buttons.get(i).getTag()));
         }
     }
 }
--- app/src/main/java/com/android/alarmy_test2/MainActivity.java
+++ app/src/main/java/com/android/alarmy_test2/MainActivity.java
@@ -1,18 +1,39 @@
 package com.android.alarmy_test2;
 
+import static java.time.DayOfWeek.*;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity {
+    Calendar calendar;
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    Boolean True = true;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint("LongLogTag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +41,34 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnItemSelectedListener(navListener);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
 
+        calendar = Calendar.getInstance();
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        // Intent gửi đi
+        Intent intentReceiver = new Intent(MainActivity.this, AlarmReceiver.class);
+        ArrayList<Boolean> myList = (ArrayList<Boolean>) getIntent().getSerializableExtra("list");
+        int hour = (int) getIntent().getIntExtra("hour", -1);
+        int minute = (int) getIntent().getIntExtra("minute", -1);
+        Log.d("----------Calendar on MainActivity-------- ", String.valueOf(calendar));
+        Log.d("----------Hour on MainActivity-------- ", String.valueOf(hour));
+        Log.d("----------Minute on MainActivity-------- ", String.valueOf(minute));
+        if (myList != null) {
+            for (int i = 0; i < myList.size(); i++) {
+                int temp = i + 1;
+                if (myList.get(i) == True) {
+                    calendar.set(Calendar.HOUR_OF_DAY, hour);
+                    calendar.set(Calendar.MINUTE, minute);
+                    calendar.set(Calendar.SECOND,0);
+                    calendar.set(Calendar.MILLISECOND,0);
+                    pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intentReceiver, PendingIntent.FLAG_UPDATE_CURRENT);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                    Log.d("Đã set", String.valueOf(calendar));
+
+                }
+            }
+        }
     }
 
     private NavigationBarView.OnItemSelectedListener navListener =
